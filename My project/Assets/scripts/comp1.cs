4d8e29d35c4bcb40f4b410ce9c4f941cb0e62e9a
andersonvalenciamontoya-lab <andersonvalenciamontoya@gmail.com>
2026-09-12 15:20:56 -0500

    Actualización
    Se añadió nuevos scripts conforme a lo especificado por la tarea de EBAC

---
+++ My project/Assets/scripts/comp1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comp1 : MonoBehaviour
+{
+    public static GameObject miObjeto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        miObjeto = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
